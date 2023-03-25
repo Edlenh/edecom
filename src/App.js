3e31f1e4127cae7b0f852d840829678b33156b5a
Edward <herreraed93@gmail.com>
2023-03-25 10:38:34 -0700

    scaffold clothing eccomerce project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+const App=()=> {
+  //initialize an array for scalablity
+  //DRY dont repeat yourself
+  const categories =[
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Women\'s',
+    },
+    {
+      id: 5,
+      title: 'Men\'s',
+    },
+  ]
 
-function App() {
   return (
-    <div className="App">
-      
+    <div className="categories-container">
+      {/* take the category array and destructre it */}
+      {/* {}=>{} expects a explicit return statement () implicit return takes place */}
+      {/* "Implicitly" means that the JS engine does it. "Explicitly" means that you must do it. */}
+
+      {categories.map(({ title })=>(
+         <div className="category-container">
+         {/* <img></img> */}
+         <div className="category-body-container">
+           <h2>{title}</h2>
+           <p>Shop Now</p>
+         </div>
+       </div>
+      ))}
     </div>
   );
 }
